dd587555238fc71cef7bc26cca0ad0d83f9c2b8f
Minifyre <minifyre@gmail.com>
2018-02-16 20:54:03 -0600

    sub test traversing
    got cherub.build to traverse entire testing tree

--- cherub.js
+++ cherub.js
@@ -6,30 +6,53 @@
 	var assertions=
 	{
 		stringify:(a,b)=>JSON.stringify(a)===JSON.stringify(b)
-	};
-	var build=function(test)
+	},
+		blank=args=>args,
+		defaults=
+		{
+			args:undefined,
+			assert:assertions.stringify,
+			cleanup:blank,
+			name:'',
+			notes:'',
+			rtn:undefined,
+			setup:blank
+		};
+	cherub.build=function(test,inherits=defaults)
 	{
-		//build path
-		var blank=args=>args,
-			defaults=
-			{
-				
-				args:undefined,
-				assert:assertions.stringify,
-				cleanup:blank,
-				name:'',
-				notes:'',
-				rtn:undefined,
-				setup:blank
-			};
-		test=Object.assign(test,defaults);
 		console.log(test);
+		if (test.tests)
+		{
+			test.tests.forEach(function(test)
+			{
+				cherub.build(test,inherits);
+			});
+		}
+		/*//build name path
+		var tests=[];			
+		test=Object.assign(defaults,test);
+		if (test.func)
+		{
+			tests.push(test);
+		}
+		if (test.tests)
+		{
+			test.tests.forEach(function(test)
+			{
+				console.log(test);
+			});
+		}
+		else
+		{
+			console.log(test);
+		}
+		return tests;*/
 	};
-	var run=function(...tests)
+	cherub.run=function(...tests)
 	{
 		tests.forEach(function(test)
 		{
-			build(test);
+			cherub.build(test);
 		});
 		/*return tests.reduce(function(results,test)
 		{
