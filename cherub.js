a3965ecb519708a6d6dff1bed63e3d1856865a49
Minifyre <minifyre@gmail.com>
2018-02-17 09:51:33 -0600

    cleaned up run()
    Simplified run() & reorganized config.

--- cherub.js
+++ cherub.js
@@ -1,73 +1,3 @@
-/*
-defaults:{hidePassed:true,parallel:true,shuffle:true},
-
-fail:function(name,time,...msg)
-{
-	cherub.totals.failed+=1;
-	cherub.output(name+': failed'+cherub.perf.report(time)+'\n',...msg);
-},
-num2percent:num=>((num*100).toFixed('2')).replace(/\.00|0$/,'')+'%',
-perf:
-{
-	now:()=>Date.now(),
-	report:time=>' (in '+time.toFixed(4)+cherub.perf.units+')',
-	units:'ms'
-},
-output:console.log,
-pass:function(test,time)
-{
-	cherub.totals.passed+=1;
-	if (cherub.hidePassed)
-	{
-		cherub.output(test+': passed'+cherub.perf.report(time));
-	}	
-},
-score:function(time)
-{
-	var {num2percent,output,totals}=cherub,
-		{failed,passed}=totals,
-		total=failed+passed,
-		percentPassed=total?num2percent(passed/total):0;
-	output(percentPassed+' Passed: '+passed+'/'+total+cherub.perf.report(time)+'\n');
-	cherub.totals={failed:0,passed:0};
-},
-shuffle:function(old)
-{
-	return old.reduce(function(arr,item,i)
-	{
-		var j=~~(Math.random()*(arr.length));
-		[arr[i],arr[j]]=[arr[j],arr[i]];
-		return arr;
-	},old.slice(0));
-},
-tests:[],
-totals:{failed:0,passed:0},
-start:function(opts={})
-{
-	var {defaults,hidePassed,next,perf,output,run,score,shuffle,tests}=cherub,
-		start=perf.now();
-	opts=Object.assign(defaults,opts);
-	output('Start Testing ('+JSON.stringify(opts)+')');
-	tests=opts.shuffle?shuffle(tests):tests;
-	return (opts.parallel?Promise.all(tests.map(run)):
-	tests.reduce((promise,test)=>promise.then(()=>run(test)),Promise.resolve()))
-	.catch(next).then(()=>score(perf.now()-start));
-}
-tests.forEach(function(test)
-{
-	var {args,assert,cleanup,func,name,notes,rtn,setup}=test,
-		{fail,next,pass,perf}=cherub,///add these
-		start=perf.now();
-	return Promise.resolve()
-	.then(setup)
-	.then(()=>args?func(...args):func())
-	.then(actual=>(assert(actual,rtn)?pass:fail)(name,perf.now()-start,actual,'!==',rtn))///pass notes here
-	.catch(err=>fail(name,perf.now()-start,err,'!==',rtn))
-	.then(cleanup)
-	.catch(next);
-});
-*/
-
 (function(args)
 {
 	'use strict';
@@ -78,15 +8,15 @@ tests.forEach(function(test)
 			equal:(actual,expected)=>actual===expected,
 			stringify:(...args)=>assertions.equal(...args.map(JSON.stringify))
 		},
-		blank=args=>args,
+		next=args=>args,
 		defaults=
 		{
 			assert:assertions.stringify,
-			cleanup:blank,
+			cleanup:next,
 			name:'',//is this necessary if it is a minimal prop?
 			notes:'',//is this necessary if it is a minimal prop?
 			rtn:undefined,
-			setup:blank
+			setup:next
 		};
 	cherub.assign=function(obj,defaults)
 	{
@@ -111,6 +41,19 @@ tests.forEach(function(test)
 		});
 		return tests;
 	};
+	cherub.config=
+	{
+		hidePassed:true,
+		output:console.log,
+		parallel:true,
+		perf:
+		{
+			now:()=>Date.now(),
+			report:time=>' (in '+time.toFixed(4)+cherub.perf.units+')',
+			units:'ms'
+		},
+		shuffle:true
+	};
 	cherub.inherit=function(test,parent)
 	{
 		var {assert,cleanup,func,rtn,setup}=parent,
@@ -121,12 +64,75 @@ tests.forEach(function(test)
 		test.name=(parent.name+'/'+test.name).replace(/^\//,'');//inherit parent's base name
 		return test;
 	};
+	cherub.num2percent=num=>((num*100).toFixed('2')).replace(/\.00|0$/,'')+'%';
 	cherub.run=function(test)
 	{
-		var tests=cherub.build(test),
+		var {build,config,fail,pass,shuffle}=cherub,
+			{perf}=config,
+			tests=build(test),
 			results={failed:0,passed:0};
+		tests=config.shuffle?shuffle(tests):tests;
 		console.log(tests);
-		///run tests
+		//reduce with a total of passed tests, failed can be infered from totals
+		tests.forEach(function(test)
+		{
+			var {args,assert,cleanup,func,name,notes,rtn,setup}=test,
+				start=perf.now();
+			return Promise.resolve()
+			.then(setup)
+			.then(()=>args?func(...args):func())//run tests
+			.then(val=>({func:assert(val,rtn)?pass:fail,val}))//eval tests
+			.catch(err=>({func:fail,val:err}))
+			.then(obj=>Object.assign(obj,{name,notes,time:perf.now()-start}))//compile info
+			.then(obj=>obj.func(obj))//report info
+			.then(cleanup)
+			.catch(next);
+		});
+	};
+	
+	function start(opts={})///TEMP
+	{
+		cherub.config.output('Start Testing');
+		return (opts.parallel?Promise.all(tests.map(run)):
+		tests.reduce((promise,test)=>promise.then(()=>run(test)),Promise.resolve()))
+		.then(()=>score(perf.now()-start));
+	}
+	
+	
+	
+	cherub.shuffle=function(old)
+	{
+		return old.reduce(function(arr,item,i)
+		{
+			var j=~~(Math.random()*(arr.length));
+			[arr[i],arr[j]]=[arr[j],arr[i]];
+			return arr;
+		},old.slice(0));
 	};
 	return cherub;
 })(typeof exports==='undefined'?[window,'cherub']:[module,'exports']);
+
+/*
+fail:function(obj)
+{
+	cherub.totals.failed+=1;
+	cherub.output(name+': failed'+cherub.perf.report(time)+'\n',...msg);
+}
+pass:function(obj)
+{
+	cherub.totals.passed+=1;
+	if (cherub.hidePassed)
+	{
+		cherub.output(test+': passed'+cherub.perf.report(time));
+	}
+}
+score:function(time)
+{
+	var {num2percent,output,totals}=cherub,
+		{failed,passed}=totals,
+		total=failed+passed,
+		percentPassed=total?num2percent(passed/total):0;
+	output(percentPassed+' Passed: '+passed+'/'+total+cherub.perf.report(time)+'\n');
+	cherub.totals={failed:0,passed:0};
+}
+*/
