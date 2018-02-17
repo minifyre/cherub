4e66402067c15d9dbf5186dcdcf96db632b33e5c
Minifyre <minifyre@gmail.com>
2018-02-16 20:36:56 -0600

    inital revamp
    initial setup of ideal inheritance-based syntax

--- cherub.js
+++ cherub.js
@@ -2,104 +2,39 @@
 {
 	'use strict';
 	var [obj,prop]=args,
-		cherub=obj[prop]=function(name='Unamed Test')
-		{
-			var {args,assert,cleanup,func,handler,setup}=cherub,
-				obj={args,assert,cleanup,func,name,rtn:'',setup};
-			cherub.tests.push(obj);
-			return new Proxy(obj,handler);
-		},
-		next=(...args)=>args,
-		assertions=
-		{
-			equal:(actual,expected)=>actual===expected,
-			stringify:(...args)=>assertions.equal(...args.map(JSON.stringify))
-		};
-	Object.assign(cherub,
+		cherub=obj[prop]={};
+	var assertions=
 	{
-		args:undefined,
-		assert:assertions.stringify,
-		assertions,
-		assign:sources=>Object.assign(cherub,sources),
-		cleanup:next,
-		defaults:{hidePassed:true,parallel:true,shuffle:true},
-		handler:
-		{
-			get:function(obj,prop)
+		stringify:(a,b)=>JSON.stringify(a)===JSON.stringify(b)
+	};
+	var build=function(test)
+	{
+		//build path
+		var blank=args=>args,
+			defaults=
 			{
-				return function(...val)
-				{
-					obj[prop]=prop==='args'?val:val[0];
-					return new Proxy(obj,cherub.handler);
-				};
-			}
-		},
-		fail:function(name,time,...msg)
+				
+				args:undefined,
+				assert:assertions.stringify,
+				cleanup:blank,
+				name:'',
+				notes:'',
+				rtn:undefined,
+				setup:blank
+			};
+		test=Object.assign(test,defaults);
+		console.log(test);
+	};
+	var run=function(...tests)
+	{
+		tests.forEach(function(test)
 		{
-			cherub.totals.failed+=1;
-			cherub.output(name+': failed'+cherub.perf.report(time)+'\n',...msg);
-		},
-		func:next,
-		num2percent:num=>((num*100).toFixed('2')).replace(/\.00|0$/,'')+'%',
-		next:next,
-		perf:
+			build(test);
+		});
+		/*return tests.reduce(function(results,test)
 		{
-			now:()=>Date.now(),
-			report:time=>' (in '+time.toFixed(4)+cherub.perf.units+')',
-			units:'ms'
-		},
-		output:console.log,
-		pass:function(test,time)
-		{
-			cherub.totals.passed+=1;
-			if (cherub.hidePassed)
-			{
-				cherub.output(test+': passed'+cherub.perf.report(time));
-			}	
-		},
-		run:function(test)
-		{
-			var {args,assert,cleanup,func,name,rtn,setup}=test,
-				{fail,next,pass,perf}=cherub,
-				start=perf.now();
-			return Promise.resolve().then(setup)
-			.then(()=>args?func(...args):func())
-			.then(actual=>(assert(actual,rtn)?pass:fail)(name,perf.now()-start,actual,'!==',rtn))
-			.catch(err=>fail(name,perf.now()-start,err,'!==',rtn))
-			.then(cleanup)
-			.catch(next);
-		},
-		score:function(time)
-		{
-			var {num2percent,output,totals}=cherub,
-				{failed,passed}=totals,
-				total=failed+passed,
-				percentPassed=total?num2percent(passed/total):0;
-			output(percentPassed+' Passed: '+passed+'/'+total+cherub.perf.report(time)+'\n');
-			cherub.totals={failed:0,passed:0};
-		},
-		setup:next,
-		shuffle:function(old)
-		{
-			return old.reduce(function(arr,item,i)
-			{
-				var j=~~(Math.random()*(arr.length));
-				[arr[i],arr[j]]=[arr[j],arr[i]];
-				return arr;
-			},old.slice(0));
-		},
-		tests:[],
-		totals:{failed:0,passed:0},
-		start:function(opts={})
-		{
-			var {defaults,hidePassed,next,perf,output,run,score,shuffle,tests}=cherub,
-				start=perf.now();
-			opts=Object.assign(defaults,opts);
-			output('Start Testing ('+JSON.stringify(opts)+')');
-			tests=opts.shuffle?shuffle(tests):tests;
-			return (opts.parallel?Promise.all(tests.map(run)):
-			tests.reduce((promise,test)=>promise.then(()=>run(test)),Promise.resolve()))
-			.catch(next).then(()=>score(perf.now()-start));
-		}
-	});
+			return results;
+		},{failed:0,passed:0});*/
+	};
+	return cherub;
 })(typeof exports==='undefined'?[window,'cherub']:[module,'exports']);
