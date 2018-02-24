5a7ba4a9806cd364db591a01e0e61211bc007652
Minifyre <minifyre@gmail.com>
2018-02-24 12:02:17 -0600

    modularized cherub
    Used ES6 modules & organized functions via SILO methodology.

--- cherub.js
+++ cherub.js
@@ -1,129 +1,142 @@
-(function(args)
+'use strict';
+var input={},
+	logic={},
+	output={},
+	config=///get into output
+	{
+		failed:'failed',
+		hidePassed:true,
+		now:()=>Date.now(),
+		output:console.log,
+		passed:'passed',
+		parallel:true,
+		precision:4,
+		shuffle:true,
+		units:'ms'
+	};
+logic.assign=function(obj,defaults)
 {
-	'use strict';
-	var [obj,prop]=args,
-		config=
-		{
-			failed:'failed',
-			hidePassed:true,
-			now:()=>Date.now(),
-			output:console.log,
-			passed:'passed',
-			parallel:true,
-			precision:4,
-			shuffle:true,
-			units:'ms'
-		},
-		cherub=obj[prop]=function(opts)
-		{
-			config=Object.assign(config,opts);
-			return cherub;
-		},
-		next=args=>args,
-		defaults=
-		{
-			assert:(actual,expected)=>JSON.stringify(actual)===JSON.stringify(expected),
-			cleanup:next,
-			name:'',
-			rtn:undefined,
-			setup:next
-		};
-	cherub.assign=function(obj,defaults)
+	return Object.keys(defaults).reduce(function(obj,prop)
 	{
-		return Object.keys(defaults).reduce(function(obj,prop)
-		{
-			!obj.hasOwnProperty(prop)?obj[prop]=defaults[prop]:'';
-			return obj;
-		},obj);
-	};
-	cherub.buildTest=function(test,inherits=defaults)
+		!obj.hasOwnProperty(prop)?obj[prop]=defaults[prop]:'';
+		return obj;
+	},obj);
+};
+logic.buildTest=function(test,inherits=defaults)
+{
+	test=logic.inherit(test,inherits);
+	var {tests}=test,
+		isNotContainer=!tests.length&&test.func;
+	return isNotContainer?[test]:logic.buildTests(tests,test);
+};
+logic.buildTests=function(children,parent)
+{
+	return children.reduce(function(tests,test)
 	{
-		test=cherub.inherit(test,inherits);
-		var {tests}=test,
-			isNotContainer=!tests.length&&test.func;
-		return isNotContainer?[test]:cherub.buildTests(tests,test);
-	};
-	cherub.buildTests=function(children,parent)
+		return tests.concat(...logic.buildTest(test,parent));
+	},[]);
+};
+logic.inherit=function(test,parent)
+{
+	var {assert,cleanup,func,rtn,setup}=parent,
+		parentProps={assert,cleanup,func,rtn,setup},
+		minProps={name:'',tests:[]};
+	test=logic.assign(test,minProps);
+	test=logic.assign(test,parentProps);
+	test.name=(parent.name+'/'+test.name).replace(/^\//,'');//prepend parent's name
+	return test;
+};
+logic.next=arg=>arg;
+logic.num2percent=num=>((num*100).toFixed(2)).replace(/\.00|0$/,'')+'%';
+logic.num2timeStr=function(time)
+{
+	var {precision,units}=config;
+	return time.toFixed(precision)+units;
+};
+logic.shuffle=function(old)
+{
+	return old.reduce(function(arr,item,i)
 	{
-		return children.reduce(function(tests,test)
-		{
-			return tests.concat(...cherub.buildTest(test,parent));
-		},[]);
-	};
-	cherub.inherit=function(test,parent)
-	{
-		var {assert,cleanup,func,rtn,setup}=parent,
-			parentProps={assert,cleanup,func,rtn,setup},
-			minProps={name:'',tests:[]};
-		test=cherub.assign(test,minProps);
-		test=cherub.assign(test,parentProps);
-		test.name=(parent.name+'/'+test.name).replace(/^\//,'');//prepend parent's name
-		return test;
-	};
-	cherub.json2msg=function(json,show=false)
-	{
-		var {err,name,rtn,time}=json,
-			{hidePassed,output}=config,
-			failed=json.hasOwnProperty('err'),
-			type=failed?'failed':'passed',
-			msg=name+' ('+cherub.reportTime(time)+') ';
-		msg=config[type]+': '+msg;
-		msg=failed?msg+' '+err+'!='+rtn:msg;
-		(failed||!hidePassed||show)?output(msg+'\n'):'';
-	};
-	cherub.num2percent=num=>((num*100).toFixed(2)).replace(/\.00|0$/,'')+'%';
-	cherub.reportTime=function(time)
-	{
-		var {precision,units}=config;
-		return time.toFixed(precision)+units;
-	};
-	cherub.run=function(...testTrees)
-	{
-		var {buildTests,json2msg,shuffle}=cherub,
-			{output,parallel,now}=config,
-			passed=0,
-			tests=buildTests(testTrees),
-			run=function(test)
-			{
-				var {args,assert,cleanup,func,name,rtn,setup}=test,
-					start=now();
-				return Promise.resolve()
-				.then(setup)
-				.then(()=>args?func(...args):func())//run tests
-				.then(val=>(assert(val,rtn)?{val}:{err:val}))//eval tests
-				.catch(err=>({err}))
-				.then(function(obj)//report info
-				{
-					var time=now()-start;
-					json2msg(Object.assign(obj,{name,rtn,time}));
-					return passed+=obj.hasOwnProperty('err')?0:1;
-				})
-				.then(cleanup)
-				.catch(next);
-			},
-			start=now();
-		tests=config.shuffle?shuffle(tests):tests;
-		return (parallel?Promise.all(tests.map(run)):
-		tests.reduce((promise,test)=>promise.then(()=>run(test)),Promise.resolve()))
-		.then(function()//score
-		{
-			var {json2msg,num2percent}=cherub,
-				time=now()-start,
-				total=tests.length,
-				failed=total-passed,//failed can be infered from totals & passed
-				percentPassed=total?num2percent(passed/total):0,
-				name=passed+'/'+total+' ('+percentPassed+')';
-			json2msg({name,time},true);
-		});
-	};
-	cherub.shuffle=function(old)
-	{
-		return old.reduce(function(arr,item,i)
-		{
-			var j=~~(Math.random()*(arr.length));
-			[arr[i],arr[j]]=[arr[j],arr[i]];
-			return arr;
-		},old.slice(0));
-	};
+		var j=~~(Math.random()*(arr.length));
+		[arr[i],arr[j]]=[arr[j],arr[i]];
+		return arr;
+	},old.slice(0));
+};
+
+
+
+
+
+
+
+
+
+
+
+var cherub=function(opts)
+{
+	config=Object.assign(config,opts);
 	return cherub;
-})(typeof exports==='undefined'?[window,'cherub']:[module,'exports']);
+},
+defaults=
+{
+	assert:(a,b)=>JSON.stringify(a)===JSON.stringify(b),
+	cleanup:logic.next,
+	name:'',
+	rtn:undefined,
+	setup:logic.next
+};
+cherub.json2msg=function(json,show=false)
+{
+	var {err,name,rtn,time}=json,
+		{hidePassed,output}=config,
+		failed=json.hasOwnProperty('err'),
+		type=failed?'failed':'passed',
+		msg=name+' ('+logic.num2timeStr(time)+') ';
+	msg=config[type]+': '+msg;
+	msg=failed?msg+' '+err+'!='+rtn:msg;
+	(failed||!hidePassed||show)?output(msg+'\n'):'';
+};
+
+cherub.run=function(...testTrees)
+{
+	var {buildTests}=logic,
+		{json2msg}=cherub,
+		{output,parallel,now}=config,
+		passed=0,
+		tests=buildTests(testTrees),
+		run=function(test)
+		{
+			var {args,assert,cleanup,func,name,rtn,setup}=test,
+				start=now();
+			return Promise.resolve()
+			.then(setup)
+			.then(()=>args?func(...args):func())//run tests
+			.then(val=>(assert(val,rtn)?{val}:{err:val}))//eval tests
+			.catch(err=>({err}))
+			.then(function(obj)//report info
+			{
+				var time=now()-start;
+				json2msg(Object.assign(obj,{name,rtn,time}));
+				return passed+=obj.hasOwnProperty('err')?0:1;
+			})
+			.then(cleanup)
+			.catch(logic.next);
+		},
+		start=now();
+	tests=config.shuffle?logic.shuffle(tests):tests;
+	return (parallel?Promise.all(tests.map(run)):
+	tests.reduce((promise,test)=>promise.then(()=>run(test)),Promise.resolve()))
+	.then(function()//score
+	{
+		var {num2percent}=logic,
+			{json2msg}=cherub,
+			time=now()-start,
+			total=tests.length,
+			failed=total-passed,//failed can be infered from totals & passed
+			percentPassed=total?num2percent(passed/total):0,
+			name=passed+'/'+total+' ('+percentPassed+')';
+		json2msg({name,time},true);
+	});
+};
+export {cherub};
