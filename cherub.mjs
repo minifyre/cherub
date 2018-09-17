b9be3cc54317276a24b5face9aff205f4f4b83bb
Minifyre <minifyre@gmail.com>
2018-09-17 16:49:54 -0500

    reorganized helper functions
    Moved util & logic funcitons directly on to cherub.

--- cherub.mjs
+++ cherub.mjs
@@ -1,16 +1,18 @@
 export default async function cherub(tests=[],opts={})
 {
 	const
-	settings=Object.assign({},config,opts),
+	settings=Object.assign({},cherub.config,opts),
 	start=settings.now()
-	if(settings.shuffle) tests=tests.reduce(util.shuffle,tests.slice())
-	const results=await logic.runTests(tests,settings)
+	if(settings.shuffle) tests=tests.reduce(cherub.shuffle,tests.slice())
+	const results=await cherub.runTests(tests,settings)
 	await settings.report({tests:results,time:settings.now()-start})
 }
-const
-logic={},
-util={},
-config=
+cherub.asyncMap=function(arr,cb)
+{
+	const chain=async (promiseArr,item)=>[...await promiseArr,await cb(item)]
+	return arr.reduce(chain,Promise.resolve([]))
+}
+cherub.config=
 {
 	assert:(a,b)=>JSON.stringify(a)===JSON.stringify(b),
 	cleanup:x=>x,
@@ -20,31 +22,36 @@ config=
 	report:function({tests,time})
 	{
 		const
-		{log}=console,
 		passed=tests.reduce((sum,x)=>sum+x.passed,0),
 		total=tests.length,
 		percent=(passed/total*100)
 				.toFixed(4)
 				.replace(/0+$/,'')
 				.replace(/\.$/,'')
-		log(`${passed}/${total} (${percent}%) passed in ${time.toFixed(4)}ms`)
+		console.log(`${passed}/${total} (${percent}%) passed in ${time.toFixed(4)}ms`)
 	},
 	reportTest:console.log,
 	shuffle:true,
 	setup:x=>x
 }
-Object.assign(cherub,{config,logic,util})
-logic.runTests=function(tests,config)
+cherub.is=(x,type)=>typeof x===type
+cherub.runTests=function(tests,config)
 {
 	const
 	{parallel}=config,
-	run=test=>logic.test(test,config)
-	return parallel?Promise.all(tests.map(run)):util.asyncMap(tests,run)
+	run=test=>cherub.test(test,config)
+	return parallel?Promise.all(tests.map(run)):cherub.asyncMap(tests,run)
 }
-logic.test=async function([actualOrFn,expectedOrFn,optsOrName={}],config)
+cherub.shuffle=function(arr,_,i)
+{
+	const j=Math.floor(Math.random()*arr.length);
+	[arr[i],arr[j]]=[arr[j],arr[i]]
+	return arr
+}
+cherub.test=async function([actualOrFn,expectedOrFn,optsOrName={}],config)
 {
 	const
-	{is}=util,
+	{is}=cherub,
 	opts=is(optsOrName,'string')?{name:optsOrName}:opts,
 	{setup,cleanup,name,now,reportTest}=Object.assign({},config,opts),
 	start=now(),
@@ -64,16 +71,4 @@ logic.test=async function([actualOrFn,expectedOrFn,optsOrName={}],config)
 	.then(x=>Object.assign({name,time:now()-start},x))
 	if(reportTest) await reportTest(result)
 	return result
-}
-util.is=(x,type)=>typeof x===type
-util.asyncMap=function(arr,cb)
-{
-	const chain=async (promiseArr,item)=>[...await promiseArr,await cb(item)]
-	return arr.reduce(chain,Promise.resolve([]))
-}
-util.shuffle=function(arr,_,i)
-{
-	const j=Math.floor(Math.random()*arr.length);
-	[arr[i],arr[j]]=[arr[j],arr[i]]
-	return arr
 }
